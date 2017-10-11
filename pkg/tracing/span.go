4d118214086fc065eda5dc866e39f1d9e4711668
Stuart Carnie <stuart.carnie@gmail.com>
2017-10-11 07:26:59 -0700

    tracing package

---
+++ pkg/tracing/span.go
@@ -0,0 +1,82 @@
+package tracing
+
+import (
+	"sync"
+
+	"time"
+
+	"github.com/influxdata/influxdb/pkg/tracing/fields"
+	"github.com/influxdata/influxdb/pkg/tracing/labels"
+)
+
+type Span struct {
+	tracer *Trace
+	mu     sync.Mutex
+	raw    RawSpan
+}
+
+type StartSpanOption interface {
+	applyStart(*Span)
+}
+
+// The StartTime start span option specifies the start time of
+// the new span rather than using now.
+type StartTime time.Time
+
+func (t StartTime) applyStart(s *Span) {
+	s.raw.Start = time.Time(t)
+}
+
+// StartSpan creates a new child span using time.Now as the start time.
+func (s *Span) StartSpan(name string, opt ...StartSpanOption) *Span {
+	return s.tracer.startSpan(name, s.raw.Context, opt)
+}
+
+// Context returns a SpanContext that can be serialized and passed to a remote node to continue a trace.
+func (s *Span) Context() SpanContext {
+	return s.raw.Context
+}
+
+func (s *Span) SetLabels(args ...string) {
+	s.mu.Lock()
+	s.raw.Labels = labels.New(args...)
+	s.mu.Unlock()
+}
+
+func (s *Span) MergeLabels(args ...string) {
+	ls := labels.New(args...)
+	s.mu.Lock()
+	s.raw.Labels.Merge(ls)
+	s.mu.Unlock()
+}
+
+func (s *Span) SetFields(set fields.Fields) {
+	s.mu.Lock()
+	s.raw.Fields = set
+	s.mu.Unlock()
+}
+
+func (s *Span) AddFields(args ...fields.Field) {
+	set := fields.New(args...)
+	s.mu.Lock()
+	s.raw.Fields.Merge(set)
+	s.mu.Unlock()
+}
+
+func (s *Span) MergeFields(set fields.Fields) {
+	s.mu.Lock()
+	s.raw.Fields.Merge(set)
+	s.mu.Unlock()
+}
+
+// Finish marks the end of the span and records it to the associated Trace.
+// If Finish is not called, the span will not appear in the trace.
+func (s *Span) Finish() {
+	s.mu.Lock()
+	s.tracer.addRawSpan(s.raw)
+	s.mu.Unlock()
+}
+
+func (s *Span) Tree() *TreeNode {
+	return s.tracer.TreeFrom(s.raw.Context.SpanID)
+}
